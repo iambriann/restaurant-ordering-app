5f4045caeae93a839b913e235ab3d66fa8ffe71c
Brian <brian.nguyen93@gmail.com>
2023-08-19 11:15:38 +1000

    Finished initial app with all required functionalities

--- index.js
+++ index.js
@@ -4,13 +4,77 @@ import { menuArray } from "./data.js"
 
 const map = new Map()
 
-function handleAddBtnClick() {
-    map.set("Pizza", 1);
+document.addEventListener('click', function(e){
+    
+    if(e.target.dataset.add) {
+        handleAddBtnClick(e.target.dataset.add)
+    } else if (e.target.dataset.remove) {
+        handleRemoveBtnClick(e.target.dataset.remove)
+    } else if (e.target.className === "complete-order-btn") {
+        handleCompleteBtnClick()
+    } else if (e.target.className === "pay-btn") {
+        handlePayBtnClick()
+    } else if (e.target.className === "cancel-btn") {
+        handleCancelBtnClick()
+    }
+}) 
+
+
+function handleAddBtnClick(itemId) {
+
+    if(document.querySelector(".thank-msg")) {
+        if(document.querySelector(".thank-msg").style.display === "flex") {
+            document.querySelector(".thank-msg").style.display="none"; 
+        }   
+    }
+    
+
+    if(map.has(itemId)) {
+        map.set(itemId, map.get(itemId)+1);
+    } else {
+        map.set(itemId, 1);
+    }
+    
+    render()
+}
+
+function handleRemoveBtnClick(itemId) {
+    if(map.has(itemId)) {
+        map.delete(itemId);
+    }
+    
+    render()
 }
 
 
+function handleCompleteBtnClick() {
+    //console.log(document.querySelector(".payment-popup").style.display === "")
+    if(document.querySelector(".payment-popup").style.display === "") {
+        document.querySelector(".payment-popup").style.display="block"; 
+    }
+}
+
+function handlePayBtnClick() {
+    map.clear()
+
+    render()
+
+    if(document.querySelector(".thank-msg").style.display === "") {
+        document.querySelector(".thank-msg").style.display="flex"; 
+    }        
+}
+
+function handleCancelBtnClick() {
+    if(document.querySelector(".payment-popup").style.display === "block") {
+        document.querySelector(".payment-popup").style.display="none"; 
+    }    
+    
+    render()
+}
+
 
 function getMenuHtml() {
+
     let menuHtml = ''
     menuArray.forEach(function(item) {
         let ingredientsSeparated = ''
@@ -29,48 +93,73 @@ function getMenuHtml() {
                 <div class="menu-emoji">${item.emoji}</div>
                 <div class="food-details">
                     <p>${item.name}</p>
-                    <p>${ingredientsSeparated}</p>
+                    <p class="ingredients-desc">${ingredientsSeparated}</p>
                     <p>$${item.price}</p>
                 </div>
             </div>
-            <img class="add-btn" alt="Add button" src="images/add-btn.png"></img>
+            <img class="add-btn" alt="Add button" data-add="${item.id}" src="images/add-btn.png"></img>
         </div>
         `
         
     })
 
-    // map.set("Pizza", 1);
-    map.set("Food", 3);
     if(map.size > 0) {
+        let totalPrice = 0
         menuHtml += 
         `
+        <div class="order">
         <h3 class="order-title">Your order</h3>
         `    
         for(const [key, value] of map) {
             menuHtml += 
             `
-            <div class="order-list">
-                <div class="order-item">
-                    <div class="order-desc">
-                        <p>${key}</p>
-                        <p>remove</p>
-                    </div>
-                    <p>${value}</p>                
+            <div class="order-item">
+                <div class="order-desc">
+                    <p>${menuArray[key].name}</p>
+                    <button class="remove-btn" data-remove="${key}">remove</button>
+                </div>
+                <div class="order-details">
+                    <p>x${value}</p>
+                    <p>$${menuArray[key].price * value}</p>                
                 </div>
             </div>
             `
+            totalPrice += menuArray[key].price * value;
         }
-        
-        
+
+        menuHtml +=
+        `
+        <hr></hr>
+        <div class="total-details">
+            <p>Total price:</p>
+            <p>$${totalPrice}</p>  
+        </div>
+        <button class="complete-order-btn">Complete order</button>
+        </div>
+        `
     }
 
-    
+    menuHtml +=
+    `
+    <div class="payment-popup">
+        <h3>Enter card details</h3>
+        <input type="text" placeholder="Enter your name" required/>
+        <input type="text" placeholder="Enter your card number" required/>
+        <input type="text" placeholder="Enter cvv" required/>
+        <button class="pay-btn">Pay</button>
+        <button class="cancel-btn">Cancel</button>
+    </div>
+
+    <div class="thank-msg">
+        <p>Thank you, your order is on its way!</p>
+    </div>
+    `
+
     return menuHtml
 }
 
 
 function render() {
-    //document.getElementById("menu-area").innerHTML = getMenuHtml()
     document.querySelector("#menu-area").innerHTML = getMenuHtml()
 }
 
